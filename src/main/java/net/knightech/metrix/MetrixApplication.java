96adab96c127b1b43f570faa89c51a2fee7bff35
pete knight <pete@knightech.net>
2019-05-06 20:28:28 +0100

    init

---
+++ src/main/java/net/knightech/metrix/MetrixApplication.java
@@ -0,0 +1,13 @@
+package net.knightech.metrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetrixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/knightech/metrix/MetrixApplicationTests.java
@@ -0,0 +1,16 @@
+package net.knightech.metrix;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MetrixApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
